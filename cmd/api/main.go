34162da6290147ea18b49e3473c3b7d1f57ad3c4
iosh <454053152@qq.com>
2022-09-26 14:24:07 +0000

    add postgresql database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const version = "1.0.0"
 
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	dbpool, err := pgxpool.New(context.Background(), cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dbpool, nil
+}
+
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -26,15 +42,23 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://root:root@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routers(),
@@ -44,7 +68,7 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	serverErr := srv.ListenAndServe()
 
-	logger.Fatal(err)
+	logger.Fatal(serverErr)
 }
---
+++ migrations/000002_add_movies_check_constraints.up.sql
@@ -0,0 +1,5 @@
+alter table movies add constraint movies_runtime_check check (runtime >= 0);
+
+alter table movies add constraint movies_year_check check (year BETWEEN 1888 AND date_part('year', now()));
+
+alter table movies add constraint genres_length_check check (array_length(genres, 1) BETWEEN 1 AND 5);
